c4e20ededc963abd84e93390c7c3b7950d5763d5
Dominic Dsa <dominicdsa35@gmail.com>
2025-09-06 16:41:06 +0530

    resolved the issue regarding clicks

--- frontend/src/components/PersonDataDashboard/CardInfo.jsx
+++ frontend/src/components/PersonDataDashboard/CardInfo.jsx
@@ -2,11 +2,25 @@ import Card from 'react-bootstrap/Card';
 import EmailSentTable from './EmailSentTable';
 
 function CardInfo({personData , setCurrEmail}) {
+  
+const calcClickRate = () => {
+  const clickedMail = personData.emailSend.filter(
+    (e) => e.clicked == true
+  ).length;
+
+  const total = personData.emailSend.length;
+
+  return total > 0 ? ((clickedMail / total) * 100).toFixed(2) + "%" : "0%";
+};
+
+
   return (
     <Card >
       <Card.Body>
         <Card.Title>Name : {personData.name}</Card.Title>
         <Card.Subtitle className="mb-2 text-muted"> G-mail Id : {personData.emailId}</Card.Subtitle>
+        <strong>Click Rate:</strong> {calcClickRate()}
+
         <EmailSentTable emailsent={personData?.emailSend || []} setCurrEmail={setCurrEmail}/>
       </Card.Body>
     </Card>
